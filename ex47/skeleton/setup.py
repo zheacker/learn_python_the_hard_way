61c6cbe2359897877d237b1597dd883d96cd6c37
Zac Heacker <zac.heacker@gmail.com>
2017-04-05 23:00:31 -0500

    ex47 & 48

---
+++ ex47/skeleton/setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'My Project',
+    'author': 'Zac Heacker',
+    'url': 'url to get it at (github)',
+    'download_url': 'where to download it (github)',
+    'author_email': 'zac.heacker@gmail.com',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['ex47'],
+    'scripts': [],
+    'name': 'projectName'
+}
+
+setup(**config)
